d43d8b43c1ce15e7e463b5f795e19c263ef872b3
nik113822 <83763478+nik113822@users.noreply.github.com>
2025-11-14 14:33:47 +0200

    Add files via upload

--- pre.py
+++ pre.py
@@ -1,75 +1,66 @@
-f=open("python.txt")
-text=f.read()
-print(text)
-from nltk import sent_tokenize
+import re
+
 import nltk
-from nltk import word_tokenize
-tokens = sent_tokenize(text)#diaspash se protaseis
-print(tokens)
-
-tokens = [word_tokenize(token) for token in tokens]#diaspash se lekseis
-print(tokens)
-
-
-#stopwords
+from nltk import sent_tokenize, word_tokenize, pos_tag
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import preprocessor as p
+
+with open("python.txt", encoding="utf-8") as f:
+    text = f.read()
+print(text)
+
+tokens = sent_tokenize(text)  # diaspash se protaseis
+print(tokens)
+
+tokens = [word_tokenize(token) for token in tokens]  # diaspash se lekseis
+print(tokens)
+
+
 stops = set(stopwords.words("english"))
-#παίρνει λίστα προτάσεων (κάθε πρόταση = λίστα tokens),
-#kai αφαιρεί από κάθε πρόταση όσα tokens είναι stopwords,
+# pairnei lista protaseon (kathe protash = lista tokens),
+# kai afairei apo kathe protash osa tokens einai stopwords,
 sentences_filtered = [[w for w in sent if w.lower() not in stops] for sent in tokens]
-print("Oi filtrarismenes portaseis einai :", sentences_filtered)
- #stemming
+print("Oi filtrarismenes portaseis einai:", sentences_filtered)
 print()
-from nltk.stem import PorterStemmer
-ps=PorterStemmer()
+
+# stemming
+ps = PorterStemmer()
 for sentence in sentences_filtered[:9]:
-    sentence_stemmed=[ps.stem(w) for w in sentence]
+    sentence_stemmed = [ps.stem(w) for w in sentence]
     print(sentence)
     print(sentence_stemmed)
 
-#lemmatization
-from nltk.stem import WordNetLemmatizer
-lemmatizer=WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
 for sentence in sentences_filtered[:9]:
-    sentence_lemmatized=[lemmatizer.lemmatize(w) for w in sentence]
+    sentence_lemmatized = [lemmatizer.lemmatize(w) for w in sentence]
     print(sentence)
-    print()
-    print("oi lemmatized protaseis einai : ", sentence_lemmatized)
+    print("oi lemmatized protaseis einai:", sentence_lemmatized)
 
-#afairesi @mentions
-import re #regular expressions
-
-# αφαιρεί @mentions όπως @giros
-clean = re.sub(r"\B@[\w.-]+", "", text)
-
+# afairei @mentions opos @giros
+# clean = re.sub(r"\B@[\w.-]+", "", text)
+p.set_options(p.OPT.MENTION)
+clean = p.clean(text)
 print(clean)
 print(text)
 
-#METATROPI THS TELEYTAIAS PROTASHS TOU TXT SE PEZA GRAMMATA
-# σπάει σε προτάσεις με ., ! ή ?
+# Metatropi tis teleytaias protasis tou txt se peza grammata
+# spaei se protaseis me ., ! i ?
 sents = re.split(r'(?<=[.!?])\s+', text)
 if sents:
-    sents[-1] = sents[-1].lower()      # τελευταία πρόταση σε πεζά
-    print(sents[-1])                   # εκτύπωση μόνο της τελευταίας
+    sents[-1] = sents[-1].lower()  # teleytaia protasi se peza
+    print(sents[-1])  # ektuposi mono tis teleytaias
 
-#afairesh url
-NEO = re.sub(r'((?:https?://|www\.)\S+)([.,!?;:]?)', r'\2', text)
-print(NEO)  
+# NEO = re.sub(r'((?:https?://|www\.)\S+)([.,!?;:]?)', r'\2', text)
+# print(NEO)
+p.set_options(p.OPT.URL)  # afairesh url
+clean = p.clean(text)
+print(clean)
 
-
-#POS tagging is assigning word types, 
-# like noun or verb,to each word.
-
-from nltk import pos_tag
-from nltk import word_tokenize
-
-text = "NLTK is great for NLP!"
-words = word_tokenize(text)
+sample_text = "NLTK is great for NLP!"
+words = word_tokenize(sample_text)
 pos_tags = pos_tag(words)
 print(pos_tags)
-
-
-
 
 
 
